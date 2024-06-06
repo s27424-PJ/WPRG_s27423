86a5c1a43f6aae61e0f7df33011b865c77ccf725
AdamRzepa <115402532+s27424-PJ@users.noreply.github.com>
2024-06-06 19:31:26 +0200

    Add files via upload

---
+++ zjazd6/1.php
@@ -0,0 +1,95 @@
+<?php
+
+class NoweAuto {
+    protected $model;
+    protected $cenaEuro;
+    protected $kursEuroPLN;
+
+    public function __construct($model, $cenaEuro, $kursEuroPLN) {
+        $this->model = $model;
+        $this->cenaEuro = $cenaEuro;
+        $this->kursEuroPLN = $kursEuroPLN;
+    }
+
+    public function obliczCene() {
+        return $this->cenaEuro * $this->kursEuroPLN;
+    }
+
+    public function getModel() {
+        return $this->model;
+    }
+
+    public function getCenaEuro() {
+        return $this->cenaEuro;
+    }
+
+    public function getKursEuroPLN() {
+        return $this->kursEuroPLN;
+    }
+}
+
+class AutoZDodatkami extends NoweAuto {
+    private $alarm;
+    private $radio;
+    private $klimatyzacja;
+
+    public function __construct($model, $cenaEuro, $kursEuroPLN, $alarm, $radio, $klimatyzacja) {
+        parent::__construct($model, $cenaEuro, $kursEuroPLN);
+        $this->alarm = $alarm;
+        $this->radio = $radio;
+        $this->klimatyzacja = $klimatyzacja;
+    }
+
+    public function obliczCene() {
+        $cenaBazowaPLN = parent::obliczCene();
+        $cenaDodatkow = $this->alarm + $this->radio + $this->klimatyzacja;
+        return $cenaBazowaPLN + $cenaDodatkow;
+    }
+
+    public function getAlarm() {
+        return $this->alarm;
+    }
+
+    public function getRadio() {
+        return $this->radio;
+    }
+
+    public function getKlimatyzacja() {
+        return $this->klimatyzacja;
+    }
+}
+
+class Ubezpieczenie extends AutoZDodatkami {
+    private $procentUbezpieczenia;
+    private $liczbaLat;
+
+    public function __construct($model, $cenaEuro, $kursEuroPLN, $alarm, $radio, $klimatyzacja, $procentUbezpieczenia, $liczbaLat) {
+        parent::__construct($model, $cenaEuro, $kursEuroPLN, $alarm, $radio, $klimatyzacja);
+        $this->procentUbezpieczenia = $procentUbezpieczenia;
+        $this->liczbaLat = $liczbaLat;
+    }
+
+    public function obliczCene() {
+        $cenaAutaZDodatkami = parent::obliczCene();
+        $wartoscUbezpieczenia = $this->procentUbezpieczenia * ($cenaAutaZDodatkami * ((100 - $this->liczbaLat) / 100));
+        return $cenaAutaZDodatkami + $wartoscUbezpieczenia;
+    }
+
+    // Gettery i Settery
+    public function getProcentUbezpieczenia() {
+        return $this->procentUbezpieczenia;
+    }
+
+    public function getLiczbaLat() {
+        return $this->liczbaLat;
+    }
+}
+
+$auto = new Ubezpieczenie("szrot", 30000, 4.5, 500, 300, 1000, 0.05, 3);
+$auto2 = new Ubezpieczenie("mercidis", 320, 5, 50, 100, 3200, 0.05, 3);
+echo "Model: " . $auto->getModel() . "\n";
+echo "Cena w PLN: " . $auto->obliczCene() . " PLN\n";
+echo "</br>";
+echo "Model: " . $auto2->getModel() . "\n";
+echo "Cena w PLN: " . $auto2->obliczCene() . " PLN\n";
+?>
